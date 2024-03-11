fd1b32bc04c0d0881e20eae2aa544b8210acc19e
ceewee12 <03230030.gcbs@rub.edu.bt>
2024-03-11 12:41:04 +0600

    first commit

---
+++ datatype.py
@@ -0,0 +1,24 @@
+x = "Hello world"
+x= 50
+x=60.5
+x = 3j
+x = ["greeks","for","greeks"]
+x = ("greeks","for","greeks")
+x = range(10)
+x = {"name": "suraj","age":24}
+x = {"greeks","for","greeks"}
+x = frozenset({"greeks","for","greeks"})
+x = True
+x = b"Greeks"
+x = bytearray(4)
+x = memoryview(bytes(6))
+
+my_list = [1,2,3,4,5,"string"]
+my_list.insert(1,5)# insert 5 at specific at index 1
+print(my_list)
+my_list.append(5)
+print(my_list)
+print(len(my_list))
+my_list.remove(2)
+varpop = my_list.pop()
+print(varpop)
