fb588afc635c4dea638a818b4771926e670b161c
ceewee12 <03230030.gcbs@rub.edu.bt>
2024-04-10 21:28:04 +0600

    commitment

---
+++ linear.py
@@ -0,0 +1,15 @@
+def linear_serach(arr,target):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+        return -1
+
+arr =[10,15,20,5,30] 
+target =30
+
+result = linear_serach(arr,target)
+if result != -1:
+    print("element found at index",result)
+
+else:
+    print("element not found")
